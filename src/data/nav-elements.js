a8d9e29f5fba99baa347249cb6d918d6e42d9f15
Zak Goedegebuur <zakgoedegebuur@gmail.com>
2023-05-05 13:33:58 +0100

    More stylistic changes

--- src/data/nav-elements.js
+++ src/data/nav-elements.js
@@ -2,6 +2,9 @@ const pageData = { pages: [
     { name: "MAPS", data: "maps lorem ipsum dipsum mipsum gypsum wipsum"},
     { name: "GOOGLE", data: "google lorem ipsum dipsum mipsum gypsum wipsum" },
     { name: "ABOUT", data: "about lorem ipsum dipsum mipsum gypsum wipsum" },
+    { name: "gmail", data: "gmail lorem ipsum dipsum mipsum gypsum wipsum" },
+    { name: "apps", data: "apps lorem ipsum dipsum mipsum gypsum wipsum" },
+    { name: "github", data: "github lorem ipsum dipsum mipsum gypsum wipsum" },
 ]};
 
 export default pageData;
